ec7da74b52035fa0e1cb129e2cff677fe3ff3c21
Vidhya Divakar <vidhyaminuteread@gmail.com>
2026-04-27 06:05:56 -0400

    Added the inital setup from the lab

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+
+import './App.css'
+
+function App() {
+  
+
+  return (
+    <>
+      <h1>Task Manager Apr 24</h1>
+    </>
+  )
+}
+
+export default App
---
+++ src/assets/components/TaskList/TaskList.tsx
@@ -0,0 +1,12 @@
+import 
+function {taskList} (){
+    return(
+        <div className = "tList">
+            <ul> Task1  </ul>
+             <ul> Task2  </ul>
+              <ul> Task3  </ul>
+        </div>
+        
+    )
+
+}6
